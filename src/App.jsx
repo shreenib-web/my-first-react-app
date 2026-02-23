16ef13a1d0361cbfd73ab4892f3b2bdf5831f427
Shreeni <shreeni@bhosale.com>
2026-02-23 12:21:25 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <h2>{count}</h2>
 
       <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      {/* <button onClick={() => setCount(count - 1)}>Decrease</button> */}
       <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
