95a21388cb475fa8e6aa87416f1327c11bf53865
Shreeni <shreeni@bhosale.com>
2026-02-23 11:45:56 +0530

    Initial commit

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+import ProductCard from "./ProductCard";
+
+function App() {
+  return (
+    <div>
+      <ProductCard title="Heidelberg Printer" price="2500000" />
+      <ProductCard title="Komori Machine" price="2200000" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/welcome.jsx
@@ -0,0 +1,6 @@
+function welcome(){
+    return(
+        <h2>welcome to react learning</h2>
+    )
+}
+export default welcome;
