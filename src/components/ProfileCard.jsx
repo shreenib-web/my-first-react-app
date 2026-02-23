0b848c0fa74dbde8c5c8b4c6e80105d290e3263a
Shreeni <shreeni@bhosale.com>
2026-02-23 15:08:00 +0530

    add paragraph

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -11,7 +11,9 @@ function ProfileCard() {
       <h2>Kiran Sharma</h2>
       <p className="title">Full Stack Developer</p>
       <p className="desc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum soluta harum asperiores eos voluptatum minima accusamus ea impedit? Ea quia ullam tenetur laborum inventore incidunt aperiam in dolorum, porro magnam.
+        Cumque repellat tempore minus natus? Quia, laboriosam dolores eius blanditiis dignissimos quam rerum. Eaque sint provident nostrum error animi, sapiente omnis laborum expedita quasi molestiae consectetur illum dignissimos dolorum dolor.
+       
       </p>
       <button className="btn">Follow</button>
     </div>
